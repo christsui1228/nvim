e82935a054c178f63c98a4e0700f86edd11c4018
christsui1228 <christsui1228@gmail.com>
2025-11-13 21:46:09 +0800

    initial commit

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,7 +17,15 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    {
+      "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+      opts = {
+        extras = {
+          "lang.python",
+        },
+      },
+    },
     -- import/override with your plugins
     { import = "plugins" },
   },
