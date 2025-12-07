13fc2d28bc5941ca2bd146c2ca9f90b5aac5d4c7
christsui1228 <christsui1228@gmail.com>
2025-12-07 18:02:03 +0800

    修改自动刷新代码修改

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,10 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- === 在下面添加你的自动刷新配置 ===
+
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  pattern = "*",
+  command = "if mode() != 'c' | checktime | endif",
+})
